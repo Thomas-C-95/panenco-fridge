791bd6d04dec5596c4566df4ab3b4763088e6f20
Thomas Callens <thomas.callens@panenco.com>
2023-09-27 15:39:06 +0200

    Fix: Product lists only returns products from user

--- src/controllers/users/handlers/getProductList.handler.ts
+++ src/controllers/users/handlers/getProductList.handler.ts
@@ -1,14 +1,12 @@
 import { RequestContext } from "@mikro-orm/core";
 import { SearchQuery } from "../../../contracts/search.query.js";
 import { Product } from "../../../entities/product.entity.js";
+import { User } from "../../../entities/user.entity.js";
 
 
-export const getProductList = (userId:string, query: SearchQuery) => {
+export const getProductList = async (userId:string, query: SearchQuery) => {
 
     const em = RequestContext.getEntityManager();
 
-    return em.findAndCount(Product, query.search? 
-                                        {type: {$ilike: `%${query.search}%`}}
-                                        :{})
-    
+    return em.findAndCount(Product, query.search? { $and: [{owner: {id: userId}}, {type: query.search}]}: {owner: {id: userId}})
 }
