cd4122ec947697c5915057603a63aa06314c6208
Thomas Callens <thomas.callens@panenco.com>
2023-09-28 12:02:47 +0200

    Fix: more efficient implementation

--- src/controllers/users/handlers/getRecipeList.handler.ts
+++ src/controllers/users/handlers/getRecipeList.handler.ts
@@ -7,10 +7,10 @@ export const getRecipeList = async (id: string) => {
 
     const em = RequestContext.getEntityManager();
 
-    const user = await em.findOne(User, id)
+    return em.findAndCount(Recipe, {owner: {id: id}})
+    // const user = await em.findOne(User, id)
 
-    await user.recipes.init();
-    const recipes = user.recipes.getItems();
-    return [recipes, recipes.length]
-
+    // await user.recipes.init();
+    // const recipes = user.recipes.getItems();
+    // return [recipes, recipes.length]
 }
