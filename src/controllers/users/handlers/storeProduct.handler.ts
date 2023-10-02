bb6763e7046ee5d2860ae648bb2bfa946d92fbd4
Thomas Callens <thomas.callens@panenco.com>
2023-10-02 08:55:26 +0200

    Feedback from review session

--- src/controllers/users/handlers/storeProduct.handler.ts
+++ src/controllers/users/handlers/storeProduct.handler.ts
@@ -1,7 +1,7 @@
 import { RequestContext } from "@mikro-orm/core";
 import { Product } from "../../../entities/product.entity.js";
 import { Fridge } from "../../../entities/fridge.entity.js";
-import { Forbidden} from "@panenco/papi";
+import { BadRequest, Forbidden} from "@panenco/papi";
 
 
 export const storeProduct = async (userId: string, productId: string, fridgeName: string) => {
@@ -13,13 +13,12 @@ export const storeProduct = async (userId: string, productId: string, fridgeName
            throw new Forbidden("notProductOwner", 'You do not own this product');
     }
 
-    const fridge = await em.findOneOrFail(Fridge, {'name': fridgeName});
+    const fridge = await em.findOneOrFail(Fridge, {'name': fridgeName}, {populate: ['contents']});
 
-    await fridge.contents.init();
     const currentStorage = fridge.contents.getItems().map(prod=>prod.size).reduce((a,b)=>a+b, 0);
 
     if (currentStorage + product.size > fridge.capacity){
-        throw new Forbidden("exceedsCapacity", 'Fridge capacity would be exceeded');
+        throw new BadRequest("exceedsCapacity", 'Fridge capacity would be exceeded');
     }
 
     fridge.contents.add(product);
